eab0d86158a5eb440459d3ef8b7399fc180943c3
Ryan <ryan@sylver.me>
2021-04-10 14:55:50 +0800

    allow matches at the start of inputs

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // properties will almost never be at the very start of the string, and if they are
 // they are usually in brackets, so we exclude matches at the very start of the string.
-export const PART_START_PATTERN = "(?<!^)(?<=\\W|\\/)";
-export const PART_END_PATTERN = "(?=\\W|$|\\/)";
+export const PART_START_PATTERN = "(?<=^|\\W|\\/)";
+export const PART_END_PATTERN = "(?=$|\\W|\\/)";
 export const SPACE_REGEX = "(?:\\.| )";
 export const SUPPORTED_MEDIA_EXTENSIONS = [".mp4", ".3gp", ".mkv", ".mov", ".wmv", ".ogg", ".flv", ".avi", ".hdv", ".webm"];
 export const SUBTITLE_FILE_EXTENSIONS = [".srt", ".sub"];
